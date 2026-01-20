e3fa6dad89d5820da2d22c0d44c56e113c7e6f55
artemcharisma <151738037+artemcharisma@users.noreply.github.com>
2026-01-20 09:19:59 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'manager-os-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/pharm.html',
+    '/training.html',
+    '/nutrition.html',
+    '/common.css',
+    '/common.js',
+    '/chart.min.js',
+    '/icon.png',
+    '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    );
+});
