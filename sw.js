ded345f4d0bef397a2edc2ef1d04417e1d792dfd
artemcharisma <151738037+artemcharisma@users.noreply.github.com>
2026-02-19 09:42:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 const CACHE_NAME = 'manager-os-v1';
 const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/pharm.html',
-    '/training.html',
-    '/nutrition.html',
-    '/assets/style.css',
-    '/assets/js/common.js',
-    '/assets/js/utils.js',
-    '/assets/js/nutrition.js',
-    '/assets/js/pharm.js',
-    '/assets/js/training.js',
-    '/icon.png',
-    '/manifest.json'
+    './',
+    './index.html',
+    './pharm.html',
+    './training.html',
+    './nutrition.html',
+    './assets/style.css',
+    './assets/training.css', /* Твій новий файл стилів */
+    './assets/js/common.js',
+    './assets/js/utils.js',
+    './assets/js/nutrition.js',
+    './assets/js/pharm.js',
+    './assets/js/training.js',
+    './icon.png',
+    './manifest.json'
 ];
-
 // Кешування під час встановлення
 self.addEventListener('install', event => {
     event.waitUntil(
