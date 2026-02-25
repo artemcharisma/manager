ded566fb75a3559e94ac59f35711640cf0c4b605
artemcharisma <151738037+artemcharisma@users.noreply.github.com>
2026-02-25 15:51:48 +0200

    Update pharm.js

--- assets/js/pharm.js
+++ assets/js/pharm.js
@@ -266,17 +266,23 @@
         },
 
 async init() {
-            // --- ВИПРАВЛЕННЯ СТРІЛОЧКИ ДЛЯ ТЕЛЕФОНУ ---
+            // --- ВИПРАВЛЕННЯ СТРІЛОЧКИ ДЛЯ IPHONE/ANDROID ---
             const style = document.createElement('style');
             style.innerHTML = `
+                /* Малюємо стрілочку прямо на фоні інпуту (темно-сіра #666, як у вашому дизайні) */
+                input[list] {
+                    background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="%23666666" viewBox="0 0 16 16"><path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/></svg>') !important;
+                    background-repeat: no-repeat !important;
+                    background-position: calc(100% - 12px) center !important;
+                    background-size: 12px !important;
+                    padding-right: 30px !important;
+                }
+                /* Ховаємо системну стрілочку, але залишаємо її клікабельною поверх нашої */
                 input[list]::-webkit-calendar-picker-indicator {
-                    display: block !important;
-                    opacity: 1 !important;
-                    color: transparent;
-                    background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="%23d4af37" viewBox="0 0 16 16"><path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/></svg>') !important;
-                    background-repeat: no-repeat;
-                    background-position: right center;
-                    cursor: pointer;
+                    opacity: 0 !important;
+                    cursor: pointer !important;
+                    width: 25px !important;
+                    height: 100% !important;
                 }
             `;
             document.head.appendChild(style);
@@ -327,7 +333,7 @@ async init() {
                     location.reload();
                 }
             };
-        }, 
+        },
 
 
         load() {
